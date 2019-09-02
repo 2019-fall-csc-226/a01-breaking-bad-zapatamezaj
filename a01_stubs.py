5fe2878fe248b13a200b2e5f136ddb0f139fd751
zapatamezaj <zapatamezaj@berea.edu>
2019-09-02 19:47:41 -0400

    Asks user for their friend's birth year and prints their Chinese Zodiac symbol. Code is finished for Task 1 and Task 2.

--- a01_stubs.py
+++ a01_stubs.py
@@ -43,17 +43,42 @@ elif my_input == '1990':
 elif my_input == '1989':
     print('HISS! You are a snake!')
 elif my_input == '1988':
-    print('You are COOL and OLD! You are a dragon')
+    print('You like castles! You are a dragon!')
 else:
     print('Try another year!')
 
 ######################################################################
 # (Required) Task 2
 # TODO Ask the user for their friend's birth year
-
+my_input_friend = input('Please enter the year your friend was born: ')
 
 # TODO Similar to above, check your friend's year using if conditionals, and print the correct animal for that year
-
+if my_input_friend == '1999':
+    print('Do you like carrots? You are a Rabbit!')
+elif my_input_friend == '1998':
+    print('ROAR! You are a tiger!')
+elif my_input_friend == '1997':
+    print('Do not have a cardi YAK attack, you are an ox!')
+elif my_input_friend == '1996':
+    print('Careful around cats, you are a rat!')
+elif my_input_friend == '1995':
+    print("OINK OINK! You are a pig")
+elif my_input_friend == '1994':
+    print('WOOF! You are a dog!')
+elif my_input_friend == '1993':
+    print('Cock-A-Doodle-Do! You are a rooster!')
+elif my_input_friend == '1992':
+    print('Do you like bananas? Because you are a monkey!')
+elif my_input_friend == '1991':
+    print("Don't eat this code! You're a goat!")
+elif my_input_friend == '1990':
+    print('Hello neigh-bor! You are a horse!')
+elif my_input_friend == '1989':
+    print('HISS! You are a snake!')
+elif my_input_friend == '1988':
+    print('You are COOL and OLD! You are a dragon')
+else:
+    print('Try another year!')
 
 ######################################################################
 # (Optional) Task 3
